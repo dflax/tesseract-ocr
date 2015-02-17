c61ab72cce2971a218ed9ad3d1e7f778a7840218
Daniel Flax <dflax@yahoo.com>
2015-02-17 15:48:00 -0500

    Added Tesseract OCR Framework.

--- LoveInASnap/LoveInASnap/ViewController.swift
+++ LoveInASnap/LoveInASnap/ViewController.swift
@@ -11,100 +11,96 @@ import UIKit
 import Foundation
 
 class ViewController: UIViewController, UITextViewDelegate, UINavigationControllerDelegate {
-  
-  @IBOutlet weak var textView: UITextView!
-  @IBOutlet weak var findTextField: UITextField!
-  @IBOutlet weak var replaceTextField: UITextField!
-  @IBOutlet weak var topMarginConstraint: NSLayoutConstraint!
-  
-  var activityIndicator:UIActivityIndicatorView!
-  var originalTopMargin:CGFloat!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-    
-    originalTopMargin = topMarginConstraint.constant
-  }
-  
-  @IBAction func takePhoto(sender: AnyObject) {
-  }
-  
-  @IBAction func swapText(sender: AnyObject) {
-    
-  }
-  
-  @IBAction func sharePoem(sender: AnyObject) {
-    
-  }
-  
-  
-  // Activity Indicator methods
-  
-  func addActivityIndicator() {
-    activityIndicator = UIActivityIndicatorView(frame: view.bounds)
-    activityIndicator.activityIndicatorViewStyle = .WhiteLarge
-    activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.25)
-    activityIndicator.startAnimating()
-    view.addSubview(activityIndicator)
-  }
-  
-  func removeActivityIndicator() {
-    activityIndicator.removeFromSuperview()
-    activityIndicator = nil
-  }
-  
-  
-  // The remaining methods handle the keyboard resignation/
-  // move the view so that the first responders aren't hidden
-  
-  func moveViewUp() {
-    if topMarginConstraint.constant != originalTopMargin {
-      return
-    }
-    
-    topMarginConstraint.constant -= 135
-    UIView.animateWithDuration(0.3, animations: { () -> Void in
-      self.view.layoutIfNeeded()
-    })
-  }
-  
-  func moveViewDown() {
-    if topMarginConstraint.constant == originalTopMargin {
-      return
-    }
 
-    topMarginConstraint.constant = originalTopMargin
-    UIView.animateWithDuration(0.3, animations: { () -> Void in
-      self.view.layoutIfNeeded()
-    })
+	@IBOutlet weak var textView: UITextView!
+	@IBOutlet weak var findTextField: UITextField!
+	@IBOutlet weak var replaceTextField: UITextField!
+	@IBOutlet weak var topMarginConstraint: NSLayoutConstraint!
 
-  }
-  
-  @IBAction func backgroundTapped(sender: AnyObject) {
-    view.endEditing(true)
-    moveViewDown()
-  }
+	var activityIndicator:UIActivityIndicatorView!
+	var originalTopMargin:CGFloat!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+
+		originalTopMargin = topMarginConstraint.constant
+	}
+
+	@IBAction func takePhoto(sender: AnyObject) {
+	}
+
+	@IBAction func swapText(sender: AnyObject) {
+	}
+
+	@IBAction func sharePoem(sender: AnyObject) {
+	}
+
+	// Activity Indicator methods
+
+	func addActivityIndicator() {
+		activityIndicator = UIActivityIndicatorView(frame: view.bounds)
+		activityIndicator.activityIndicatorViewStyle = .WhiteLarge
+		activityIndicator.backgroundColor = UIColor(white: 0, alpha: 0.25)
+		activityIndicator.startAnimating()
+		view.addSubview(activityIndicator)
+	}
+
+	func removeActivityIndicator() {
+		activityIndicator.removeFromSuperview()
+		activityIndicator = nil
+	}
+
+	// The remaining methods handle the keyboard resignation/
+	// move the view so that the first responders aren't hidden
+
+	func moveViewUp() {
+		if topMarginConstraint.constant != originalTopMargin {
+			return
+		}
+
+		topMarginConstraint.constant -= 135
+		UIView.animateWithDuration(0.3, animations: { () -> Void in
+			self.view.layoutIfNeeded()
+		})
+	}
+
+	func moveViewDown() {
+		if topMarginConstraint.constant == originalTopMargin {
+			return
+		}
+
+		topMarginConstraint.constant = originalTopMargin
+		UIView.animateWithDuration(0.3, animations: { () -> Void in
+			self.view.layoutIfNeeded()
+		})
+	}
+
+	@IBAction func backgroundTapped(sender: AnyObject) {
+		view.endEditing(true)
+		moveViewDown()
+	}
 }
 
 extension ViewController: UITextFieldDelegate {
-  func textFieldDidBeginEditing(textField: UITextField) {
-    moveViewUp()
-  }
-  
-  @IBAction func textFieldEndEditing(sender: AnyObject) {
-    view.endEditing(true)
-    moveViewDown()
-  }
-  
-  func textViewDidBeginEditing(textView: UITextView) {
-    moveViewDown()
-  }
+	func textFieldDidBeginEditing(textField: UITextField) {
+		moveViewUp()
+	}
+
+	@IBAction func textFieldEndEditing(sender: AnyObject) {
+		view.endEditing(true)
+		moveViewDown()
+	}
+
+	func textViewDidBeginEditing(textView: UITextView) {
+		moveViewDown()
+	}
 }
 
 extension ViewController: UIImagePickerControllerDelegate {
-
 }
+
+
